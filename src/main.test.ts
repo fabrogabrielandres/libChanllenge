b784e0d1a0efaec5148d0334aac0611b94efed0c
gabreil <fabrogabrielandres@gmail.com>
2025-03-29 11:18:26 +0100

    refactor scoreboard functionality to use createMatch method and enhance game validation

--- src/main.test.ts
+++ src/main.test.ts
@@ -1,32 +1,41 @@
 import { createScoreBoard } from "./main";
 
 describe("ScoreBoard", () => {
-  let { startGame } = createScoreBoard();
+  let { createMatch, getGames, resetGames } = createScoreBoard();
 
   beforeEach(() => {
     createScoreBoard();
   });
 
   test("Test 1 --->  Create new Game", () => {
-    let game = startGame("Argentina", "Germany");
+    let game = createMatch("ArgenTina", "Germany");
     expect(game.localTeam).toBe("argentina");
     expect(game.visitTeam).toBe("germany");
   });
-  test("Test 2 --->  Create new with teams some space at the end , or at the final  ", () => {
-    let game = startGame("  ArgeNtina", "  GErmaNy   ");
-    expect(game.localTeam).toBe("argentina");
-    expect(game.visitTeam).toBe("germany");
+
+  test("Test 2 --->  Create new game with teams some space at the end , or at the final  ", () => {
+    let game = createMatch("BelGica ", "  Holanda");
+    expect(game.localTeam).toBe("belgica");
+    expect(game.visitTeam).toBe("holanda");
   });
+
   test("Test 3 --->  Try to insert empty values  ", () => {
-    
-    expect(() => startGame("Portugal", "")).toThrow(
+    expect(() => createMatch("Portugal", "")).toThrow(
       "Home and away teams cannot be the same"
     );
-    expect(() => startGame("c","")).toThrow(
+    expect(() => createMatch("c", "")).toThrow(
       "Home and away teams cannot be the same"
     );
-    expect(() => startGame("","")).toThrow(
+    expect(() => createMatch("", "")).toThrow(
       "Home and away teams cannot be the same"
     );
+  });
+
+  test("Test 4 --->  insert teams already playing  ", () => {
+    resetGames();
+    createMatch("Bulgaria", "Mexico");
+    expect(() => {
+      createMatch("Team A", "Mexico");
+    }).toThrow("Team A is already in a match");
   });
 });
--- src/main.ts
+++ src/main.ts
@@ -1,27 +1,66 @@
 interface CreateScoreBoardInterface {
-  startGame: (
-    local: string,
-    visit: string
-  ) => {
-    startedGame?: Date;
-    localTeam: string;
-    visitTeam: string;
-  };
+  createMatch: (local: string, visit: string) => GameInterface;
+  getGames: () => Array<GameInterface>;
+  resetGames: () => void;
+}
+
+interface GameInterface {
+  startedGame?: Date;
+  localTeam: string;
+  visitTeam: string;
 }
 
 export const createScoreBoard = (): CreateScoreBoardInterface => {
+  let games: Array<GameInterface> = [];
+
+  const findTeamInGames = (local: string, visit: string) => {
+    let isPlaying = games.some(
+      (game) =>
+        game.localTeam === local ||
+        game.localTeam === visit ||
+        game.visitTeam === local ||
+        game.visitTeam === visit
+    );
+    return isPlaying;
+  };
+
   const startGame = (local: string, visit: string) => {
+    let localTeam = local.toLocaleLowerCase().trim();
+    let visitTeam = visit.toLocaleLowerCase().trim();
     if (!local || !visit) {
-        throw new Error('Home and away teams cannot be the same');
+      throw new Error("Home and away teams cannot be the same");
     }
-    return {
+
+    let isPlaying = findTeamInGames(local, visit);
+
+    if (isPlaying) {
+      throw new Error("Team A is already in a match");
+    }
+
+    let newGame = {
       startedGame: new Date(),
-      localTeam: local.toLocaleLowerCase().trim(),
-      visitTeam: visit.toLocaleLowerCase().trim(),
+      localTeam,
+      visitTeam,
     };
+    return newGame;
+  };
+
+  const createMatch = (local: string, visit: string) => {
+    let localTeam = local.toLocaleLowerCase().trim();
+    let visitTeam = visit.toLocaleLowerCase().trim();
+    const newgames = startGame(localTeam, visitTeam);
+    games = [...games, newgames];
+    return newgames;
+  };
+
+  const getGames = () => games;
+  const resetGames = () => {
+    games = [];
   };
 
   return {
-    startGame,
+    getGames,
+    createMatch,
+    resetGames
   };
 };
